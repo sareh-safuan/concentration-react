7823f560cbb60b5372f130315ab3462ce866a908
Sareh Safuan <sarehsafuan@gmail.com>
2020-05-29 16:26:07 +0800

    Updates

---
+++ precache-manifest.80cab7178c28214a82892e12b7288c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe68869826303d49ddab2a6b0a91676",
+    "url": "/concentration-react/index.html"
+  },
+  {
+    "revision": "99ed8a4bcda5638ab0c1",
+    "url": "/concentration-react/static/css/main.9a403560.chunk.css"
+  },
+  {
+    "revision": "a48bfeebfa6377563b65",
+    "url": "/concentration-react/static/js/2.c36ea92d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/concentration-react/static/js/2.c36ea92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ed8a4bcda5638ab0c1",
+    "url": "/concentration-react/static/js/main.2ada6f48.chunk.js"
+  },
+  {
+    "revision": "33dec6b44ee21ff743c1",
+    "url": "/concentration-react/static/js/runtime-main.f76298d5.js"
+  }
+]);
